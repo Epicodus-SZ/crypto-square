1884bc1b3a71dcb6c570e67c7538994240ac4d24
Joel Bakken and Steve Zaske <steve@>
2017-06-08 15:42:44 -0700

    add code to finish spec 5c

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//
+
 var items = [
   [1, 2, 3],
   [4, 5, 6],
@@ -6,24 +8,25 @@ var items = [
 
 //first number is row and the second is colomn
 
-
 function encrypt(originalString) {
   //convert to all lowercase
   var results = originalString.toLowerCase().replace(/\s/g, "").replace(/[.,\/#!$?%\^&\*;:{}=\-_`~()]/g,"");
-var columns = Math.ceil(Math.sqrt(originalString.length));
-var rows = Math.ceil(originalString.length / columns);
-var total = columns*rows;
-console.log("stringlength:"+originalString.length + "/" + total + " columns:" + columns + "rows:" + rows);
+  var columns = Math.ceil(Math.sqrt(originalString.length));
+  var rows = Math.ceil(originalString.length / columns);
+  var total = columns*rows;
+  //create an array of row items each item having column items inside it.
+  var cryptoTable = [];
+  //lets create the crypto table
+  for (var rowIndex=0; rowIndex < originalString.length; rowIndex=rowIndex+columns) {
+    //create the array (which has column data inside)
+    cryptoTable.push(results.substring(rowIndex,rowIndex+columns).split(""));
+  };
+  console.log(cryptoTable);
+
+  console.log("stringlength:"+originalString.length + "/" + total + " columns:" + columns + "rows:" + rows);
 
   return results;
 }
-console.log(encrypt("Joel bakken was HERE,  a#* nd he did a good job? What an aweson partner he has!"));
-console.log(encrypt("Joe bakken was HERE,  a#* nd he did a good job?"));
-console.log(encrypt("Joel ken was HERE,  a#* nd he did a good job?"));
-console.log(encrypt("Joel was HERE,  a#* nd he did a good job?"));
-console.log(encrypt("Joel s HERE,  a#* nd he did a good job?"));
-console.log(encrypt("Joel E,  a#* nd he did a good job?"));
-console.log(encrypt("Joel a#* nd he did a good job?"));
-console.log(encrypt("Joel he did a good job?"));
-console.log(encrypt("Joel he did a goodrr job?"));
-console.log(encrypt("Joel a good job?"));
+
+
+console.log(encrypt("abcdefghijklmnopqrstuvwxyz56012345601234560123456"));
