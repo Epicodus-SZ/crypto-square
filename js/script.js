b5d09ab1dddda43ca3d082fff859a3a007041492
Joel Bakken and Steve Zaske <steve@>
2017-06-08 15:59:06 -0700

    add code to finish spec item 6a

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-//
-
-var items = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-];
-
-//first number is row and the second is colomn
-
 function encrypt(originalString) {
   //convert to all lowercase
   var results = originalString.toLowerCase().replace(/\s/g, "").replace(/[.,\/#!$?%\^&\*;:{}=\-_`~()]/g,"");
@@ -16,12 +6,23 @@ function encrypt(originalString) {
   var total = columns*rows;
   //create an array of row items each item having column items inside it.
   var cryptoTable = [];
+  var encodedString = ""
   //lets create the crypto table
   for (var rowIndex=0; rowIndex < originalString.length; rowIndex=rowIndex+columns) {
     //create the array (which has column data inside)
     cryptoTable.push(results.substring(rowIndex,rowIndex+columns).split(""));
   };
-  console.log(cryptoTable);
+
+  //encryption code goes here
+    for (var colCount = 0;colCount < columns;colCount++){
+      for (var rowCount = 0;rowCount < cryptoTable.length;rowCount++){
+        encodedString = encodedString + (cryptoTable[rowCount][colCount]);
+      //  console.log(cryptoTable[rowCount][colCount]);
+
+    }
+  }
+  console.log(encodedString);
+  
 
   console.log("stringlength:"+originalString.length + "/" + total + " columns:" + columns + "rows:" + rows);
 
