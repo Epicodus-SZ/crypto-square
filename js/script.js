ec6b426c86e3c1976ac4ed17e18b6ac8bf82a9ca
Joel Bakken and Steve Zaske <joel@>
2017-06-08 14:21:16 -0700

    add code to finish spec 5a and 5b

--- js/script.js
+++ js/script.js
@@ -10,6 +10,20 @@ var items = [
 function encrypt(originalString) {
   //convert to all lowercase
   var results = originalString.toLowerCase().replace(/\s/g, "").replace(/[.,\/#!$?%\^&\*;:{}=\-_`~()]/g,"");
+var columns = Math.ceil(Math.sqrt(originalString.length));
+var rows = Math.ceil(originalString.length / columns);
+var total = columns*rows;
+console.log("stringlength:"+originalString.length + "/" + total + " columns:" + columns + "rows:" + rows);
+
   return results;
 }
-console.log(encrypt("Joel bakken was HERE,  a#* nd he did a good job?"))
+console.log(encrypt("Joel bakken was HERE,  a#* nd he did a good job? What an aweson partner he has!"));
+console.log(encrypt("Joe bakken was HERE,  a#* nd he did a good job?"));
+console.log(encrypt("Joel ken was HERE,  a#* nd he did a good job?"));
+console.log(encrypt("Joel was HERE,  a#* nd he did a good job?"));
+console.log(encrypt("Joel s HERE,  a#* nd he did a good job?"));
+console.log(encrypt("Joel E,  a#* nd he did a good job?"));
+console.log(encrypt("Joel a#* nd he did a good job?"));
+console.log(encrypt("Joel he did a good job?"));
+console.log(encrypt("Joel he did a goodrr job?"));
+console.log(encrypt("Joel a good job?"));
