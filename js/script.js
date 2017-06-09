fd29c96a305b75cefe5c8e7044355eafe04e0e1b
szaske <github@zaske.com>
2017-06-08 19:38:32 -0700

    fix bug on last crypto-table row returning undefined

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 function encrypt(originalString) {
   //convert to all lowercase
-  debugger;
-  var results = originalString.toLowerCase().replace(/\s/g, "").replace(/[.,\/#!$?%\^&\*;:{}=\-_`~()]/g,"");
+  var results = originalString.toLowerCase().replace(/\s/g, "").replace(/[.,\/#!$?%\^&\*;:{}=\-_`~()]/g, "");
   var columns = Math.ceil(Math.sqrt(results.length));
   var rows = Math.ceil(results.length / columns);
-  var total = columns*rows;
+  var total = columns * rows;
   //create an array of row items each item having column items inside it.
   var cryptoTable = [];
   var encodedString = "";
 
   //lets create the crypto table
-  for (var rowIndex=0; rowIndex < results.length; rowIndex=rowIndex+columns) {
+  for (var rowIndex = 0; rowIndex < results.length; rowIndex = rowIndex + columns) {
     //create the array (which has column data inside)
-    cryptoTable.push(results.substring(rowIndex,rowIndex+columns).split(""));
+    cryptoTable.push(results.substring(rowIndex, rowIndex + columns).split(""));
   };
   //encryption code goes here
-  for (var colCount = 0;colCount < columns;colCount++){
-    for (var rowCount = 0;rowCount < cryptoTable.length;rowCount++){
-      //bug here ..fix by checking for undefined and breaking out
-      encodedString = encodedString + (cryptoTable[rowCount][colCount]);
+  for (var colCount = 0; colCount < columns; colCount++) {
+    for (var rowCount = 0; rowCount < cryptoTable.length; rowCount++) {
+      //need to check for undefined because the last row is not always full
+      if (typeof cryptoTable[rowCount][colCount] != 'undefined') {
+        encodedString = encodedString + (cryptoTable[rowCount][colCount]);
+      } else break;
+    }
   }
-  }
-  //Incrementing by 6 because our code adds and extra space to the string each loop
-  for (var charCount = 0;charCount < encodedString.length;charCount+=6) {
+  //Incrementing by 6 because our code adds and extra space to the string each loop.
+  //Starts at 5 so we don't add a space on the front of the string
+  for (var charCount = 5; charCount < encodedString.length; charCount += 6) {
     encodedString = encodedString.slice(0, charCount) + " " + encodedString.slice(charCount);
   }
-
-  console.log("Original String:" + originalString, "stringlength:"+originalString.length + "/" + total + " columns:" + columns + "rows:" + rows, "encoded results: " + encodedString);
-
+  console.log("Original String:" + originalString + "\nEncoded Results:" + encodedString + "\nString length:" + originalString.length + "\nColumns:" + columns + " - Rows:" + rows);
   return results;
 }
 
